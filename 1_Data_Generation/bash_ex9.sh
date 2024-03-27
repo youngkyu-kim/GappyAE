f035ae9f0152066d864a99726252c713a16e12b1
youngkyu-kim <25357221+youngkyu-kim@users.noreply.github.com>
2024-03-27 11:42:57 +0900

    Add files via upload

--- 1_Data_Generation/bash_ex9.sh
+++ 1_Data_Generation/bash_ex9.sh
@@ -13,6 +13,7 @@ MFEM_DIR="../dependencies/mfem/examples"
 cp lasdi_ex9.cpp $MFEM_DIR
 cp makefile_ex9 "$MFEM_DIR"
 cp interp_to_numpy_9.py "$MFEM_DIR"
+cp periodic-square_ex9.mesh ../dependencies/mfem/data
 
 cd "$MFEM_DIR"
 rm ex9_interp_*.npz
@@ -22,6 +23,7 @@ make lasdi_ex9 -f makefile_ex9
 rm -rf ~/LaSDI/Radial\ Advection/timeit_FOM.txt
 
 for i in $(seq 75 1 125)
+#for i in $(seq 75 1 75)
 do
 	echo $i >> ~/LaSDI/Radial\ Advection/timeit_FOM.txt
         rm -rf ex9_sim/* 
@@ -34,7 +36,8 @@ do
 	# 	./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -r 3 -tf 3 -dt 5.0e-3 -vs 1 -freq $((i/100)).$((i%100)) -visit
 	# fi
 	start_time=$(date +%s.%3N)
-	./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -r 3 -tf 3 -dt 5.0e-3 -vs 1 -freq $((i/100)).$((i%100)) -visit
+	#./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -o 1 -r 5 -s 4 -tf 3 -dt 5.0e-3 -vs 1 -freq $( echo "scale=2; $i/100" | bc ) -visit
+	./lasdi_ex9 -m ../data/periodic-square_ex9.mesh -p 3 -o 2 -r 4 -s 4 -tf 3 -dt 5.0e-3 -vs 1 -freq $( echo "scale=2; $i/100" | bc ) -visit
 	end_time=$(date +%s.%3N)
 	echo "scale=3; $end_time - $start_time" | bc >> ~/LaSDI/Radial\ Advection/timeit_FOM.txt	
 	mv -f Example9* ex9_sim
