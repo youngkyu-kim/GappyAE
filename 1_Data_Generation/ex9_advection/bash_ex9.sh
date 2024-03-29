dccb70772470ceb4f44427a687087929e5c183c7
HyeokminLee <132337225+HyeokminLee@users.noreply.github.com>
2024-03-29 16:07:29 +0900

    Update bash_ex9.sh

--- 1_Data_Generation/ex9_advection/bash_ex9.sh
+++ 1_Data_Generation/ex9_advection/bash_ex9.sh
@@ -20,18 +20,18 @@ rm ex9_interp_*.npz
 rm -rf ex9_sim
 mkdir ex9_sim
 make lasdi_ex9 -f makefile_ex9
-rm -rf ~/LaSDI/Radial\ Advection/timeit_FOM.txt
+rm -rf ~/LaSDI/ex9_advection/timeit_FOM.txt
 
 for i in $(seq 75 1 125)
 #for i in $(seq 75 1 75)
 do
-	echo $i >> ~/LaSDI/Radial\ Advection/timeit_FOM.txt
+	echo $i >> ~/LaSDI/ex9_advection/timeit_FOM.txt
         rm -rf ex9_sim/* 
 	# if [ $i = 100 ]; then
 	# 	start_time=$(date +%s.%3N)
 	# 	./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -r 3 -tf 3 -dt 5.0e-3 -vs 1 -freq $((i/100)).$((i%100)) -visit
 	# 	end_time=$(date +%s.%3N)
-	# 	echo "scale=3; $end_time - $start_time" | bc > ~/LaSDI/Radial\ Advection/timeit_FOM.txt
+	# 	echo "scale=3; $end_time - $start_time" | bc > ~/LaSDI/ex9_advection/timeit_FOM.txt
 	# else
 	# 	./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -r 3 -tf 3 -dt 5.0e-3 -vs 1 -freq $((i/100)).$((i%100)) -visit
 	# fi
@@ -39,7 +39,7 @@ do
 	#./lasdi_ex9 -m ../data/periodic-square.mesh -p 3 -o 1 -r 5 -s 4 -tf 3 -dt 5.0e-3 -vs 1 -freq $( echo "scale=2; $i/100" | bc ) -visit
 	./lasdi_ex9 -m ../data/periodic-square_ex9.mesh -p 3 -o 2 -r 4 -s 4 -tf 3 -dt 5.0e-3 -vs 1 -freq $( echo "scale=2; $i/100" | bc ) -visit
 	end_time=$(date +%s.%3N)
-	echo "scale=3; $end_time - $start_time" | bc >> ~/LaSDI/Radial\ Advection/timeit_FOM.txt	
+	echo "scale=3; $end_time - $start_time" | bc >> ~/LaSDI/ex9_advection/timeit_FOM.txt	
 	mv -f Example9* ex9_sim
     mv -f ex9*gf ex9_sim
 	mv -f ex9.mesh ex9_sim
@@ -47,7 +47,7 @@ do
 done
 
 make clean
-cd ../../../Radial\ Advection/
+cd ../../../ex9_advection/
 rm -rf data
 mkdir data
 mv -f $MFEM_DIR/ex9_interp_*.npz ./data/
