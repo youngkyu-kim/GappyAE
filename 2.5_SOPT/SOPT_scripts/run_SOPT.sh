2c4c2dff0ec93434656567506eaa9bdaf0c4281e
youngkyu-kim <25357221+youngkyu-kim@users.noreply.github.com>
2024-04-04 17:08:25 +0900

    Add files via upload

---
+++ 2.5_SOPT/SOPT_scripts/run_SOPT.sh
@@ -0,0 +1,10 @@
+sudo mkdir -p SOPT_sample_points
+
+sudo python3 ex9_advection_AE_SOPT.py
+sudo python3 ex9_advection_POD_SOPT.py
+
+sudo python3 ex16_diffusion_AE_SOPT.py
+sudo python3 ex16_diffusion_POD_SOPT.py
+
+sudo python3 ex23_wave_AE_SOPT.py
+sudo python3 ex23_wave_POD_SOPT.py
